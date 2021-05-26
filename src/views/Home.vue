d49d34364667246ea1fbc4510e3439f2fd60e020
iigmir <roc120j@gmail.com>
2021-05-26 22:19:09 +0800

    txt

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png" />
+        <div class="v">
+            <p>If Element is successfully added to this project, you'll see an <code v-text="'<el-button>'"></code> below</p>
+            <el-button>el-button</el-button>
+        </div>
     </div>
 </template>
 
