1f861ca5a8fb1755b490e171818d870151ac15df
iigmir <roc120j@gmail.com>
2021-05-26 22:16:14 +0800

    install

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,36 @@
 <template>
-    <div id="app">
-        <navigator />
-        <router-view/>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
     </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import Navigator from "./components/Navigator";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-    name: "App",
-    components: {
-        Navigator
-    },
+  name: 'app',
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
-<style lang="scss">
+<style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
-
-#nav {
-    padding: 30px;
-
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-
-        &.router-link-exact-active {
-            color: #42b983;
-        }
-    }
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
