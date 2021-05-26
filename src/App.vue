92c90343f9dc5c4ba2c98c1a2e635df1f7ef56ea
iigmir <roc120j@gmail.com>
2021-05-26 22:08:11 +0800

    4

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,40 @@
 <template>
-  <div id="app">
-    <navigator />
-    <router-view/>
-  </div>
+    <div id="app">
+        <navigator />
+        <router-view/>
+    </div>
 </template>
 
 <script>
 import Navigator from "./components/Navigator";
 
 export default {
-  name: "App",
-  components: {
-    Navigator
-  },
+    name: "App",
+    components: {
+        Navigator
+    },
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+    padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
