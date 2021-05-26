37a5547155d24a910d3e7a36db3373e251a37ad1
iigmir <roc120j@gmail.com>
2021-05-26 22:21:29 +0800

    del

--- src/main.js
+++ src/main.js
@@ -3,6 +3,5 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import i18n from './i18n'
-import './plugins/element.js'
 
 createApp(App).use(i18n).use(store).use(router).mount('#app')
