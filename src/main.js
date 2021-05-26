f25fb3dc73be45ab8d3bebac0f67ade74427291f
iigmir <roc120j@gmail.com>
2021-05-26 22:27:34 +0800

    re-install

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import i18n from './i18n'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 
-createApp(App).use(i18n).use(store).use(router).mount('#app')
+createApp(App).use(i18n).use(store).use(router).use(ElementPlus).mount('#app')
