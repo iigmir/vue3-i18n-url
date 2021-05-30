7a1cec723c32160cae65411205e237b33cc43c17
iigmir <roc120j@gmail.com>
2021-05-30 22:59:39 +0800

    i18n loads

--- src/views/i18n.vue
+++ src/views/i18n.vue
@@ -24,13 +24,11 @@ export default {
     },
     setup() {
         const options = [
-            { value: 'Option1', label: 'Option1' },
-            { value: 'Option2', label: 'Option2' },
-            { value: 'Option3', label: 'Option3' },
-            { value: 'Option4', label: 'Option4' },
-            { value: 'Option5', label: 'Option5' },
+            { value: "en", label: "English" },
+            { value: "zh-Hant", label: "繁體中文" },
+            { value: "ko", label: "한국어" },
         ];
-        const value = ref("Option3");
+        let value = ref("en");
         return { options, value };
     },
 }
