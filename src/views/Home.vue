4dbe34135023159d449c94688295c46c0db2b47a
iigmir <roc120j@gmail.com>
2021-05-30 22:46:37 +0800

    Indients

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="home">
+    <main class="home">
         <img alt="Vue logo" src="../assets/logo.png" />
         <div class="v">
             <p>If Element is successfully added to this project, you'll see an <code v-text="'<el-button>'"></code> below</p>
             <el-button>el-button</el-button>
         </div>
         <p>e-<a href="https://element-plus.org/#/en-US">Elemenygdsgsdg</a></p>
-    </div>
+    </main>
 </template>
 
 <script>
--- src/views/i18n.vue
+++ src/views/i18n.vue
@@ -1,17 +1,37 @@
 <template>
     <main>
-        <h1>Foobar</h1>
+        <h1>Language select</h1>
+        <el-select v-model="value" placeholder="Select">
+            <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+            />
+        </el-select>
         <the-panel />
     </main>
 </template>
 
 <script>
+import { ref } from "vue";
 import ThePanel from "@/components/HelloI18n.vue";
 
 export default {
     name: "i18n",
     components: {
         ThePanel
+    },
+    setup() {
+        const options = [
+            { value: 'Option1', label: 'Option1' },
+            { value: 'Option2', label: 'Option2' },
+            { value: 'Option3', label: 'Option3' },
+            { value: 'Option4', label: 'Option4' },
+            { value: 'Option5', label: 'Option5' },
+        ];
+        const value = ref("Option3");
+        return { options, value };
     },
 }
 </script>
