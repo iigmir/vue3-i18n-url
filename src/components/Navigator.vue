4b58624448b861128d704ad4bed63eba3c41571e
iigmir <roc120j@gmail.com>
2021-05-30 22:35:11 +0800

    i18n

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -4,10 +4,10 @@
             <i class="el-icon-star-on"></i>Home
         </el-menu-item>
         <el-menu-item index="/about">
-            <i class="el-icon-star-on"></i>About
+            <i class="el-icon-place"></i>About
         </el-menu-item>
         <el-menu-item index="/i18n">
-            <i class="el-icon-star-on"></i>i18n
+            <i class="el-icon-chat-round"></i>i18n
         </el-menu-item>
     </el-menu>
 </template>
