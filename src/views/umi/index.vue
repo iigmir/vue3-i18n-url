cf2d6691ab825c9794e7703ce66703253f9c684f
iigmir <roc120j@gmail.com>
2021-06-19 12:27:44 +0800

    Add another i18n

---
+++ src/views/umi/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <p>{{ t("umi") }}</p>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+export default defineComponent({
+    name: "Flowersi18n",
+    setup() {
+        const { t } = useI18n({ inheritLocale: true, useScope: "local", });
+        // Something todo ..
+        return { t }
+    }
+})
+</script>
+
+<i18n>
+{
+    "en": {
+        "umi": "We are the flowers of the future"
+    },
+    "ja": {
+        "umi": "私たちは未来の花"
+    },
+    "zh-Hant": {
+        "umi": "我們是未來的花"
+    },
+    "ko": {
+        "umi": "우리들은 미래의 꽃"
+    }
+}
+</i18n>
