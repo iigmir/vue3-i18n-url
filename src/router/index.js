65521de209a9c728868f368a8a03df8273795806
iigmir <roc120j@gmail.com>
2021-06-19 13:17:17 +0800

    An magic i18n..............?

--- src/router/index.js
+++ src/router/index.js
@@ -3,31 +3,34 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 
 const routes = [
-    [
-        {
-            path: "/:locale/",
-            name: "Home",
-            component: Home
-        },
-        {
-            path: "/:locale/about",
-            name: "About",
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
-        },
-        {
-            path: "/:locale/i18n",
-            name: "i18n",
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "i18n" */ "../views/i18n/index.vue")
-        }
-    ],
+    {
+        path: "/:locale/",
+        name: "Home",
+        component: Home
+    },
+    {
+        path: "/:locale/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
+    },
+    {
+        path: "/:locale/i18n",
+        name: "i18n",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "i18n" */ "../views/i18n/index.vue")
+    },
+    {
+        path: "/",
+        redirect: "/en"
+    }
 ];
 
+// eslint-disable-next-line
 const i18n_url = [
         {
     path: "/:locale",
