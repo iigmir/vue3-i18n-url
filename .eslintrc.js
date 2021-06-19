8a556b004cf50e6581bb2bd90666744674357281
iigmir <roc120j@gmail.com>
2021-06-19 12:31:44 +0800

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     "semi": ["error", "always"],
-    "quote": ["error", "double", { "avoidEscape": true, "allowTemplateLiterals": true }]
+    "quotes": ["error", "double", { "avoidEscape": true, "allowTemplateLiterals": true }]
   },
   overrides: [
     {
