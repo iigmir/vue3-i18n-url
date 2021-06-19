396ad0b9d6804a7877f7875bba6b058259b62e4f
iigmir <roc120j@gmail.com>
2021-06-19 14:46:23 +0800

    refactor routes

--- src/components/Navigator/index.vue
+++ src/components/Navigator/index.vue
@@ -8,30 +8,35 @@
 
 <script>
 import { useI18n } from "vue-i18n";
+import { computed, watchEffect } from "vue";
 
 export default {
     name: "Navigator",
     computed: {},
     setup() {
         const { locale } = useI18n({ useScope: "global" });
+        // const language = ref( locale.value );
         const i18n_route = path => `/${locale.value}${path}`;
-        const routes = [
-            {
-                index: i18n_route("/"),
-                icon: ["el-icon-star-on"],
-                name: "Home"
-            },
-            {
-                index: i18n_route("/about"),
-                icon: ["el-icon-place"],
-                name: "About"
-            },
-            {
-                index: i18n_route("/i18n"),
-                icon: ["el-icon-chat-round"],
-                name: "i18n"
-            },
-        ];
+        const routes = computed( () =>
+            ([
+                {
+                    index: i18n_route("/"),
+                    icon: ["el-icon-star-on"],
+                    name: "Home"
+                },
+                {
+                    index: i18n_route("/about"),
+                    icon: ["el-icon-place"],
+                    name: "About"
+                },
+                {
+                    index: i18n_route("/i18n"),
+                    icon: ["el-icon-chat-round"],
+                    name: "i18n"
+                },
+            ]))
+        ;
+        watchEffect(() => console.log(locale.value));
         return { routes };
     }
 };
