de520740b4a6da601d266e9c65cd27c6a16f2c40
iigmir <roc120j@gmail.com>
2021-06-19 13:07:31 +0800

    i18n url failed

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,16 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import SupportLanguages from "@/locales/support-languages.json";
+// import SupportLanguages from "@/locales/support-languages.json";
 import Home from "@/views/Home.vue";
 
-const routes = [{
-    path: "/:locale",
-    component: { template: "<router-view></router-view>" },
-    beforeEnter: (to, from, next) => {
-        const locale = to.params.locale;
-        debugger;
-        // console.log(locale);
-        if (!SupportLanguages.includes(locale)) return next("en");
-        // If the locale is supported and the current locale is different, we switch to the chosen language.
-        // if (i18n.locale !== locale) { // 4
-        //     i18n.locale = locale;
-        // }
-        return next();
-    },
-    children: [
+const routes = [
+    [
         {
-            path: "/",
+            path: "/:locale/",
             name: "Home",
             component: Home
         },
         {
-            path: "/about",
+            path: "/:locale/about",
             name: "About",
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
@@ -31,7 +18,7 @@ const routes = [{
             component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
         },
         {
-            path: "/i18n",
+            path: "/:locale/i18n",
             name: "i18n",
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
@@ -39,10 +26,27 @@ const routes = [{
             component: () => import(/* webpackChunkName: "i18n" */ "../views/i18n/index.vue")
         }
     ],
-},{
-    path: "/:pathMatch(.*)",
-    redirect() { return "/en"; }
-}];
+//     {
+//     path: "/:locale",
+//     component: { template: "<router-view></router-view>" },
+//     beforeEnter: (to, from, next) => {
+//         const locale = to.params.locale;
+//         // debugger;
+//         // console.log(locale);
+//         if (!SupportLanguages.includes(locale)) return next("en");
+//         // If the locale is supported and the current locale is different, we switch to the chosen language.
+//         // if (i18n.locale !== locale) { // 4
+//         //     i18n.locale = locale;
+//         // }
+//         return next();
+//     },
+//     children,
+// }
+// ,{
+//     path: "/",
+//     redirect: "/en",
+// }
+];
 
 const router = createRouter({
     history: createWebHashHistory(),
