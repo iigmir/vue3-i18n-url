b3cc4d20496b5f956f182208485a21c3ae179bd0
iigmir <roc120j@gmail.com>
2021-06-19 12:59:07 +0800

    Refactor

--- src/router/index.js
+++ src/router/index.js
@@ -2,35 +2,13 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import SupportLanguages from "@/locales/support-languages.json";
 import Home from "@/views/Home.vue";
 
-const children = [
-    {
-        path: "/",
-        name: "Home",
-        component: Home
-    },
-    {
-        path: "/about",
-        name: "About",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
-    },
-    {
-        path: "/i18n",
-        name: "i18n",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "i18n" */ "../views/i18n/index.vue")
-    }
-];
-
 const routes = [{
     path: "/:locale",
     component: { template: "<router-view></router-view>" },
     beforeEnter: (to, from, next) => {
         const locale = to.params.locale;
+        debugger;
+        // console.log(locale);
         if (!SupportLanguages.includes(locale)) return next("en");
         // If the locale is supported and the current locale is different, we switch to the chosen language.
         // if (i18n.locale !== locale) { // 4
@@ -38,9 +16,31 @@ const routes = [{
         // }
         return next();
     },
-    children,
+    children: [
+        {
+            path: "/",
+            name: "Home",
+            component: Home
+        },
+        {
+            path: "/about",
+            name: "About",
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
+        },
+        {
+            path: "/i18n",
+            name: "i18n",
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "i18n" */ "../views/i18n/index.vue")
+        }
+    ],
 },{
-    path: "*",
+    path: "/:pathMatch(.*)",
     redirect() { return "/en"; }
 }];
 
