f023e4631fff3bf4f22ea9ee7b7ecf7bd8d1e577
iigmir <roc120j@gmail.com>
2021-06-19 16:07:40 +0800

    add watch

--- src/i18n-route.vue
+++ src/i18n-route.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script>
+import { ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 
 export default {
@@ -11,6 +12,9 @@ export default {
         const [ lang ] = window.location.hash.split("/").filter( text => text !== "#" );
         const { locale } = useI18n({ useScope: "global" });
         locale.value = lang;
+        watch( ref(locale.value), (newval, oldval) => {
+            console.log(newval, oldval);
+        });
     }
 };
 </script>
