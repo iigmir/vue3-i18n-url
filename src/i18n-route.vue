7ebddcbf05c2f468e253ca33c28cad5cad0f97bb
iigmir <roc120j@gmail.com>
2021-06-20 23:13:47 +0800

    Refactor

--- src/i18n-route.vue
+++ src/i18n-route.vue
@@ -3,16 +3,17 @@
 </template>
 
 <script>
+import { useRoute } from "vue-router";
 import { ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 
 export default {
     name: "i18nMount",
     setup() {
-        const [ lang ] = window.location.hash.split("/").filter( text => text !== "#" );
+        const lang = useRoute().params.locale ?? "en";
         const { locale } = useI18n({ useScope: "global" });
         locale.value = lang;
-        watch( ref(locale.value), (newval, oldval) => {
+        watch( ref(lang), (newval, oldval) => {
             console.log(newval, oldval);
         });
     }
